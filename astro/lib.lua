c6627438488a9cc86972b7c9ac434c2fa289a722
call me vikram or pain <Vikramjit2354@gmail.com>
2025-10-20 12:39:33 +0530

    Update lib.lua

--- astro/lib.lua
+++ astro/lib.lua
@@ -1,29 +1,4 @@
-local api = {} -- main module
-local apiname = "astro"
+local a
+a = {}
 
-api.errors = {}
-
--- simple pcall
-api.errors.pcall = function(a)
-    local ok, err = pcall(a.func, table.unpack(a.args or {}))
-    if not ok and a.printerror then
-        print(apiname..".pcall: error")
-        print(err)
-    end
-    return ok, err
-end
-
--- xpcall with traceback
-api.errors.xpcall = function(a)
-    local function errHandler(err)
-        if a.printerror then
-            print(apiname..".xpcall: error")
-            print(err)
-        end
-        return err
-    end
-    local ok, err = xpcall(function() a.func(table.unpack(a.args or {})) end, errHandler)
-    return ok, err
-end
-
-return api
+return a
